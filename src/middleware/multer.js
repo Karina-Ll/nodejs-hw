6025afb242d3d0d180d81f0b57401af9a3c0b338
Karina <karina24032006.09@gmail.com>
2026-06-20 15:19:42 +0300

    fix: use Joi from celebrate to avoid version conflict

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -4,6 +4,7 @@ import createHttpError from 'http-errors';
 const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
+  console.log('FILE INFO:', file.mimetype, file.originalname);
   if (!file.mimetype.startsWith('image/')) {
     return cb(createHttpError(400, 'Only images allowed'));
   }
