38639e7a592836d2d35d140f78a113749b9ae9bc
Karina <karina24032006.09@gmail.com>
2026-06-20 14:53:07 +0300

    fix: use Joi from celebrate to avoid version conflict

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,18 +6,23 @@ import { createSession, setSessionCookies } from '../services/auth.js';
 import { catchAsync } from '../utils/catchAsync.js';
 
 export const registerUser = catchAsync(async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const existingUser = await User.findOne({ email });
-  if (existingUser) throw createHttpError(400, 'Email in use');
+    const existingUser = await User.findOne({ email });
+    if (existingUser) throw createHttpError(400, 'Email in use');
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({ email, password: hashedPassword });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ email, password: hashedPassword });
 
-  const session = await createSession(user._id);
-  setSessionCookies(res, session);
+    const session = await createSession(user._id);
+    setSessionCookies(res, session);
 
-  res.status(201).json(user);
+    res.status(201).json(user);
+  } catch (err) {
+    console.error('REGISTER ERROR:', err);
+    throw err;
+  }
 });
 
 export const loginUser = catchAsync(async (req, res) => {
